697ada68fd2d1ac1107ee96be415fbfca47c8979
Nicole Bond <nicbond22@gmail.com>
2025-04-13 11:41:21 -0400

    Initial Commit

---
+++ GitPracticeTwo/GitPracticeTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeTwoApp.swift
+//  GitPracticeTwo
+//
+//  Created by Scholar on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
