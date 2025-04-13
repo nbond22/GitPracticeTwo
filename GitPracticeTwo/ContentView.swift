bec5f75d8747563c72c16da3d7bc55acd92e9867
Nicole Bond <nicbond22@gmail.com>
2025-04-13 11:43:02 -0400

    Added Change 2 to ContentView

--- GitPracticeTwo/ContentView.swift
+++ GitPracticeTwo/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, everybody!")
             Text("Change 1")
+            Text("Change 2")
         }
         .padding()
     }
